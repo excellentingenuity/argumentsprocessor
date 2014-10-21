6a8065fe0ffe567f617a1570c4aef72a01b9e6ea
James Johnson <james.johnson@excellentingenuity.com>
2014-10-21 16:28:50 -0400

    updated liscense and setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,18 @@ from distutils.core import setup
 setup(
   name = 'argumentsprocessor',
   packages = ['argumentsprocessor'], # this must be the same as the name above
-  version = '1.0.0',
+  version = '1.0.4',
   description = 'An arguments processor',
   author = 'James Johnson',
   author_email = 'james.johnson@excellentingenuity.com',
   url = 'https://github.com/excellentingenuity/argumentsprocessor', # use the URL to the github repo
   download_url = 'https://github.com/excellentingenuity/argumentsprocessor.git', # I'll explain this in a second
   keywords = ['arguments', 'processing'], # arbitrary keywords
-  classifiers = [],
+  liscense = 'BSD3',
+  classifiers = [
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Liscense :: BSD3',
+    'Development Status :: Production',
+  ],
 )
