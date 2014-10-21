d07443b7a0398565ab828d9f1dfd5c96e846f181
James Johnson <james.johnson@excellentingenuity.com>
2014-10-21 16:52:34 -0400

    setup.py, version throught and readme into longdescription

--- argumentsprocessor/argumentsprocessor.py
+++ argumentsprocessor/argumentsprocessor.py
@@ -91,7 +91,7 @@ class ArgumentsProcessor(object):
     """
 
     __author__ = 'James Johnson'
-    __version__ = '1.0.0'
+    __version__ = '1.0.4'
 
     supplied_arguments = {}
     expected_arguments = {}
--- setup.py
+++ setup.py
@@ -1,19 +1,45 @@
 from distutils.core import setup
+long_description = ''
+
+try:
+    import subprocess
+    import pandoc
+
+    process = subprocess.Popen(
+        ['which pandoc'],
+        shell=True,
+        stdout=subprocess.PIPE,
+        universal_newlines=True
+    )
+
+    pandoc_path = process.communicate()[0]
+    pandoc_path = pandoc_path.strip('\n')
+
+    pandoc.core.PANDOC_PATH = pandoc_path
+
+    doc = pandoc.Document()
+    doc.markdown = open('README.md').read()
+
+    long_description = doc.rst
+
+except:
+    pass
 setup(
   name = 'argumentsprocessor',
   packages = ['argumentsprocessor'], # this must be the same as the name above
   version = '1.0.4',
   description = 'An arguments processor',
+  long_description=long_description,
   author = 'James Johnson',
   author_email = 'james.johnson@excellentingenuity.com',
   url = 'https://github.com/excellentingenuity/argumentsprocessor', # use the URL to the github repo
   download_url = 'https://github.com/excellentingenuity/argumentsprocessor.git', # I'll explain this in a second
   keywords = ['arguments', 'processing'], # arbitrary keywords
-  liscense = 'BSD3',
+  license = 'BSD New',
   classifiers = [
     'Programming Language :: Python :: 3.3',
     'Programming Language :: Python :: 3.4',
-    'Liscense :: BSD3',
+    'License :: BSD New',
     'Development Status :: Production',
   ],
 )
