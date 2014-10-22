7103275fd5a51ca309323206ec0b82453c1a0751
James Johnson <james.johnson@excellentingenuity.com>
2014-10-22 09:57:29 -0400

    adding change file, and method to generate documnetation from markdown

--- setup.py
+++ setup.py
@@ -22,14 +22,26 @@ try:
     doc.markdown = open('README.md').read()
 
     long_description = doc.rst
+    #converts markdown readme to reStructured
+    z = pypandoc.convert('README','rst',format='markdown')
 
+    #writes converted file
+    with open('README.rst','w') as outfile:
+        outfile.write(z)
+
+    #converts markdown license to reStructured
+    z = pypandoc.convert('LICENSE','rst',format='markdown')
+
+    #writes converted file
+    with open('LICENSE.rst','w') as outfile:
+        outfile.write(z)
 except:
     pass
 setup(
   name = 'argumentsprocessor',
   packages = ['argumentsprocessor'], # this must be the same as the name above
   version = argumentsprocessor.__version__,
-  description = 'An arguments processor',
+  description = 'A Python module that process a dictionary of arguments against a supplied dictionary of expected arguments.',
   long_description=long_description,
   author = 'James Johnson',
   author_email = 'james.johnson@excellentingenuity.com',
