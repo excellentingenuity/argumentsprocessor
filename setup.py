88ecbce94114c428b2f2ef9cf1370c11673a9375
James Johnson <james.johnson@excellentingenuity.com>
2014-10-22 10:54:43 -0400

    updated setup.py to remove unecessary processing of README

--- setup.py
+++ setup.py
@@ -1,42 +1,7 @@
 from distutils.core import setup
 import argumentsprocessor
-long_description = ''
+long_description = open('README.rst').read()
 
-try:
-    import subprocess
-    import pandoc
-
-    process = subprocess.Popen(
-        ['which pandoc'],
-        shell=True,
-        stdout=subprocess.PIPE,
-        universal_newlines=True
-    )
-
-    pandoc_path = process.communicate()[0]
-    pandoc_path = pandoc_path.strip('\n')
-
-    pandoc.core.PANDOC_PATH = pandoc_path
-
-    doc = pandoc.Document()
-    doc.markdown = open('README.md').read()
-
-    long_description = doc.rst
-    #converts markdown readme to reStructured
-    z = pypandoc.convert('README','rst',format='markdown')
-
-    #writes converted file
-    with open('README.rst','w') as outfile:
-        outfile.write(z)
-
-    #converts markdown license to reStructured
-    z = pypandoc.convert('LICENSE','rst',format='markdown')
-
-    #writes converted file
-    with open('LICENSE.rst','w') as outfile:
-        outfile.write(z)
-except:
-    pass
 setup(
   name = 'argumentsprocessor',
   packages = ['argumentsprocessor'], # this must be the same as the name above
