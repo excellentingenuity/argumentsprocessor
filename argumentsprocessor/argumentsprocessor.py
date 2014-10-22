a935694852c9eb0886e29d3996b53d5f20602e48
James Johnson <james.johnson@excellentingenuity.com>
2014-10-22 15:03:56 -0400

    cleanup and version bump to fix travis.ci build issue, cleanup Fixes #3

--- argumentsprocessor/argumentsprocessor.py
+++ argumentsprocessor/argumentsprocessor.py
@@ -91,7 +91,7 @@ class ArgumentsProcessor(object):
     """
 
     __author__ = 'James Johnson'
-    __version__ = '1.1.2'
+    __version__ = '1.1.3'
 
     supplied_arguments = {}
     expected_arguments = {}
