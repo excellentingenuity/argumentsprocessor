2624e56c51cda2021b71560860bba14beb624e5c
James Johnson <james.johnson@excellentingenuity.com>
2014-10-22 20:57:12 -0400

    adding long description

--- argumentsprocessor/argumentsprocessor.py
+++ argumentsprocessor/argumentsprocessor.py
@@ -91,7 +91,7 @@ class ArgumentsProcessor(object):
     """
 
     __author__ = 'James Johnson'
-    __version__ = '1.1.4'
+    __version__ = '1.1.5'
 
     supplied_arguments = {}
     expected_arguments = {}
